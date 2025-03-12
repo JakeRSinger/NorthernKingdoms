c77c386962daf28e2442a10ab65b76cb0b62f81c
Jake Singer <21011375@uhi.ac.uk>
2025-03-12 20:36:01 +0000

    Version 12032025-2
    Changed cataloguing layout

--- README.md
+++ README.md
@@ -3,7 +3,17 @@
 
 
 Version 12032025 - 1
-Added cataloguing front end with database driven population.
-Need to implement insertion and updating.
+- Added cataloguing front end with database driven population.
+
+- Need to implement insertion and updating.
                 (Select a new item, site or location field to create a new one.)
                 (Add new dig site system to match location.)
+
+Version 12032025 - 2
+- Changed formatting of cataloguing page. 
+- Added dig site and artefact id selections.
+- Added the new ID selections as defaults.
+
+- Need to add image upload.
+- Need to implement backend for cataloguing.
+
--- nk-site/scripts/catalogue.js
+++ nk-site/scripts/catalogue.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', fetchLocations);
+window.addEventListener('load', fetchDigs);
 
 async function fetchLocations() {
     try {
@@ -31,6 +32,42 @@ function showLocations(locations) {
             locationOption.innerText = location.location_id;
             locationOption.value = location.location_id;
             locationSelect.appendChild(locationOption);
+        });
+    }
+}
+
+
+async function fetchLocations() {
+    try {
+        // Fetch digs
+        const response = await fetch("https://20.108.25.134/nk-webservice/digs.php");
+        
+        if (!response.ok) {
+            throw new Error(`Error fetching digs: ${response.status}`);
+        }
+
+        const digs = await response.json();
+
+        if (digs.Error) {
+            console.error("Error in response:", digs.Error);
+        } else {
+            showDigs(digs);
+        }
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+}
+
+function showDigs(digs) {
+    const digSelect = document.getElementById("artefact_dig_site_no");
+
+    if (digSelect) {
+
+        digs.forEach(dig => {
+            const digOption = document.createElement('option');
+            digOption.innerText = dig.dig_site_no;
+            digOption.value = dig.dig_site_no;
+            digOption.appendChild(digOption);
         });
     }
 }
