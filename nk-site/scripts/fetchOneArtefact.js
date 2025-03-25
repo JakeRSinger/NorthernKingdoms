ea267d436764b3a0ec80525eac4663854453e721
Jake Singer <21011375@uhi.ac.uk>
2025-03-25 14:53:22 +0000

    Version 25032025-3.3.2
    Removed jQuery from artefact viewing.

--- nk-site/scripts/fetchOneArtefact.js
+++ nk-site/scripts/fetchOneArtefact.js
@@ -20,24 +20,24 @@ async function artefactSelection(artefactID) {
 }
 
 function writeArtefacts(artefact) {
-
     if (!artefact) return;
 
-    $("#artefact_date_found").innerText = (artefact.artefact_date_found || "");
-    $("#artefact_broad_subperiod").innerText = (artefact.artefact_broad_subperiod || "");
-    $("#artefact_date_earliest").innerText = (artefact.artefact_date_earliest || "");
-    $("#artefact_date_latest").innerText = (artefact.artefact_date_latest || "");
-    $("#artefact_weight").innerText = (artefact.artefact_weight || "");
-    $("#artefact_height").innerText = (artefact.artefact_height || "");
-    $("#artefact_length").innerText = (artefact.artefact_length || "");
-    $("#artefact_breadth").innerText = (artefact.artefact_breadth || "");
-    $("#artefact_classification").innerText = (artefact.artefact_classification || "");
-    $("#artefact_functional_group").innerText = (artefact.artefact_functional_group || "");
-    $("#artefact_material").innerText = (artefact.artefact_material || "");
-    $("#artefact_decorative_style").innerText = (artefact.artefact_decorative_style || "");
-    $("#artefact_desc").innerText = (artefact.artefact_desc || "");
-    $('#artefact_location_id').innerText = (artefact.artefact_location_id || "");
-    $('#artefact_dig_site_no').innerText = (artefact.artefact_dig_site_no || "");
-    $('#artefact_image').attr('src', artefact.artefact_image || "https://20.108.25.134/NorthernKingdoms/nk-site/img/nk-logo.jpeg");
+    document.getElementById("artefact_date_found").innerText = artefact.artefact_date_found || "";
+    document.getElementById("artefact_broad_subperiod").innerText = artefact.artefact_broad_subperiod || "";
+    document.getElementById("artefact_date_earliest").innerText = artefact.artefact_date_earliest || "";
+    document.getElementById("artefact_date_latest").innerText = artefact.artefact_date_latest || "";
+    document.getElementById("artefact_weight").innerText = artefact.artefact_weight || "";
+    document.getElementById("artefact_height").innerText = artefact.artefact_height || "";
+    document.getElementById("artefact_length").innerText = artefact.artefact_length || "";
+    document.getElementById("artefact_breadth").innerText = artefact.artefact_breadth || "";
+    document.getElementById("artefact_classification").innerText = artefact.artefact_classification || "";
+    document.getElementById("artefact_functional_group").innerText = artefact.artefact_functional_group || "";
+    document.getElementById("artefact_material").innerText = artefact.artefact_material || "";
+    document.getElementById("artefact_decorative_style").innerText = artefact.artefact_decorative_style || "";
+    document.getElementById("artefact_desc").innerText = artefact.artefact_desc || "";
+    document.getElementById("artefact_location_id").innerText = artefact.artefact_location_id || "";
+    document.getElementById("artefact_dig_site_no").innerText = artefact.artefact_dig_site_no || "";
 
+    // Set image source
+    document.getElementById("artefact_image").src = artefact.artefact_image || "https://20.108.25.134/NorthernKingdoms/nk-site/img/nk-logo.jpeg";
 }
