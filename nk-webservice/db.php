1ce69c0019249e11d93d3abd4f31543a30361a27
Jake Singer <21011375@uhi.ac.uk>
2025-03-26 16:13:43 +0000

    Update db.php

--- nk-webservice/db.php
+++ nk-webservice/db.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host = "20.108.25.134";
-    $dbName = "NorthernKingdoms";
-    $username = "Admin_21011375";
-    $password = 'Th3@dm1nPas5';
+    $host = "";
+    $dbName = "";
+    $username = "";
+    $password = '';
 
 
     try {
